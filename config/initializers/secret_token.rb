b04e509c4ef0638c8dd7eb57399049db68ba5084
ryosuke <ryosuke_iwama@hotmail.com>
2014-09-16 21:26:30 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jtravel::Application.config.secret_key_base = 'e42c9f3f0635ffbcab9b902391d773195bafd5038e9805df19a28f42f41ad2c55f75ea7d157e4215474389521d4c2d57e94e2226d69d06ecd2e265cedfe7b73f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jtravel::Application.config.session_store :cookie_store, key: '_jtravel_session'
